08f92e69227d1debb5b07a6e18e486c10827a7fd
MartinBINARD <martin.binard.dev@gmail.com>
2025-07-11 22:34:10 -1000

    Add leaflet map with tailwind for angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-map',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'leaflet-angular';
+export class AppComponent implements AfterViewInit {
+  private map!: L.Map;
+
+  ngAfterViewInit(): void {
+    this.initialiserCarte();
+  }
+
+  private initialiserCarte(): void {
+    this.map = L.map('map').setView([-17.525040113732356, -149.52056762883547], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+  }
 }
